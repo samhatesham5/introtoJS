b5b5b80e7be9769299742aeda354ab6de9f46a96
Sam <samanthawhitley5@gmail.com>
2021-11-05 09:53:41 -0400

    pic for another project

--- questionThree.js
+++ questionThree.js
@@ -1,12 +1,11 @@
 //Questions 3//
 
-/*
+
 console.log ('Plug in your Initals :P')
 
 //Creating the function 'addInitals'//
 function addInitals(first, last){
-  console.log ('Your Initals:' + (first + last));
+  console.log ('Your Initals:', (first + last));
 }
 
 addInitals ('S', 'W');
-*/
